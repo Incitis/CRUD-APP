0bf510bc78cf1ec69b3fd7ec0457c1a3f8e7203b
Océane Vignot <82438041+Incitis@users.noreply.github.com>
2023-03-06 10:19:07 +0100

    router commit

--- server/index.js
+++ server/index.js
@@ -1,3 +1,13 @@
 const express = require("express");
+const port = 5000;
 
 const app = express();
+
+//~~ Middleware pour traiter les données de la request
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/post", require("./router/post.routes"));
+
+//~~ Lancer le serveur
+app.listen(port, () => console.log("Le serveur est good" + port));
